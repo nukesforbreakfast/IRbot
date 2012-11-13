2cd4e44696e5a42efdfcd1c9ebec14b470e44791
Ryan Causey <rccausey@gmail.com>
2012-11-13 15:42:57 -0800

    Finished first prototype of IRtransmittest code and preparing to test.

--- IRtransmittest.c
+++ IRtransmittest.c
@@ -10,6 +10,21 @@
 #include <avr/AVRXlib/AVRX_Clocks.h>
 #include <avr/AVRXlib/AVRX_Serial.h>
 
+ISR(TCC1_OVF_vect)
+{
+	if(TCC1_PER == 150)
+	{
+		TCC0_CTRLA = TC_CLKSEL_OFF_gc;
+		TCC1_PER = 75;
+	}
+	else
+	{
+		TCC0_CTRLA = TC_CLKSEL_DIV256_gc;
+		TCC1_PER = 150;
+	}	
+}
+
+
 void main(void)
 {
 	unsigned long sClk, pClk;
@@ -25,13 +40,35 @@ void main(void)
 	*/
 	PMIC_CTRL = PMIC_HILVLEN_bm | PMIC_MEDLVLEN_bm | PMIC_LOLVLEN_bm; //enable all levels of interrupts
 	
+	/*
+	* Timer port C0 configuration
+	*/
+	TCC0_CTRLA = TC_CLKSEL_DIV256_gc; //set prescaler to clk/256
+	TCC0_CTRLB = 0x10 | TC_WGMODE_SS_gc; //turn on capture(CCAEN) and set waveform generation mode to PWM
+	TCC0_CTRLC = 0x00; //turn off compares
+	TCC0_CTRLD = 0x00; //turn off events
+	TCC0_CTRLE = 0x00; //turn off byte mode
+	TCC0_PER = 4; //set the top of the period
+	TCC0_CCA = 2; //set the compare register value to achieve 0% duty cycle at start
 	
+	/*
+	* Timer port C1 configuration
+	*/
+	TCC1_CTRLA = TC_CLKSEL_DIV256_gc; //set prescaler to clk/256
+	TCC1_CTRLB = TC_WGMODE_NORMAL_gc; //set waveform generation mode to normal
+	TCC1_CTRLC = 0x00; //turn off compares
+	TCC1_CTRLD = 0x00; //turn off events
+	TCC1_CTRLE = 0x00; //turn off byte mode
+	TCC1_PER = 150; //set the top of the period
+	TCC1_INTCTRLA = 0x01; //set timer c1 overflow interrupts to low level
+	
+	/*
+	* Port C configuration
+	*/
+	PORTC_DIR = 0xFF;
 	
 	sei();
-	
-	PORTH_DIR = 0xFF;
-	PORTH_OUT = 0xAA;
-	
+		
     while(1)
     {
         ;
