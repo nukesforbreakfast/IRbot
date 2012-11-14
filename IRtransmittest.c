aba5d4442014d6f0648d85dd89b7a842cbb8d0f6
Ryan Causey <rccausey@gmail.com>
2012-11-13 16:37:47 -0800

    Final IRtransmittest code. The values used in the TCC0-PER and TCC0-CCA in this commit are the ones we want to use from now on to achieve 38khz oscillation of the LED.

--- IRtransmittest.c
+++ IRtransmittest.c
@@ -12,15 +12,15 @@
 
 ISR(TCC1_OVF_vect)
 {
-	if(TCC1_PER == 150)
+	if(TCC1_PER == 600)
 	{
 		TCC0_CTRLA = TC_CLKSEL_OFF_gc;
-		TCC1_PER = 75;
+		TCC1_PER = 300;
 	}
 	else
 	{
-		TCC0_CTRLA = TC_CLKSEL_DIV256_gc;
-		TCC1_PER = 150;
+		TCC0_CTRLA = TC_CLKSEL_DIV64_gc;
+		TCC1_PER = 600;
 	}	
 }
 
@@ -43,29 +43,31 @@ void main(void)
 	/*
 	* Timer port C0 configuration
 	*/
-	TCC0_CTRLA = TC_CLKSEL_DIV256_gc; //set prescaler to clk/256
+	TCC0_CTRLA = TC_CLKSEL_DIV64_gc; //set prescaler to clk/64
 	TCC0_CTRLB = 0x10 | TC_WGMODE_SS_gc; //turn on capture(CCAEN) and set waveform generation mode to PWM
 	TCC0_CTRLC = 0x00; //turn off compares
 	TCC0_CTRLD = 0x00; //turn off events
 	TCC0_CTRLE = 0x00; //turn off byte mode
-	TCC0_PER = 4; //set the top of the period
-	TCC0_CCA = 2; //set the compare register value to achieve 0% duty cycle at start
+	TCC0_PER = 12; //set the top of the period
+	TCC0_CCA = 6; //set the compare register value to achieve 50% duty cycle at start
 	
 	/*
 	* Timer port C1 configuration
 	*/
-	TCC1_CTRLA = TC_CLKSEL_DIV256_gc; //set prescaler to clk/256
+	TCC1_CTRLA = TC_CLKSEL_DIV64_gc; //set prescaler to clk/64
 	TCC1_CTRLB = TC_WGMODE_NORMAL_gc; //set waveform generation mode to normal
 	TCC1_CTRLC = 0x00; //turn off compares
 	TCC1_CTRLD = 0x00; //turn off events
 	TCC1_CTRLE = 0x00; //turn off byte mode
-	TCC1_PER = 150; //set the top of the period
+	TCC1_PER = 600; //set the top of the period
 	TCC1_INTCTRLA = 0x01; //set timer c1 overflow interrupts to low level
 	
 	/*
 	* Port C configuration
 	*/
 	PORTC_DIR = 0xFF;
+	PORTH_DIR = 0xFF;
+	PORTH_OUT = 0x00;
 	
 	sei();
 		
