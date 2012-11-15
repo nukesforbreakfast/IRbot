9ea12ae89f45f5a435f793c72674d50b1cbfd54b
Ryan Causey <rccausey@gmail.com>
2012-11-14 21:07:54 -0800

    Completed writing code for testing the transmitter/reciever.

--- IRtransmittest.c
+++ IRtransmittest.c
@@ -10,8 +10,18 @@
 #include <avr/AVRXlib/AVRX_Clocks.h>
 #include <avr/AVRXlib/AVRX_Serial.h>
 
+volatile XUSARTst serialStruct;
+volatile int is1;
+volatile int temp;
+
 ISR(TCC1_OVF_vect)
 {
+	/* for when we use serial
+	TCC0_CTRLA = TC_CLKSEL_OFF_gc;
+	TCC1_CTRLA = TC_CLKSEL_OFF_gc;
+	*/
+	
+	//repeatedly send 1's
 	if(TCC1_PER == 600)
 	{
 		TCC0_CTRLA = TC_CLKSEL_OFF_gc;
@@ -21,13 +31,35 @@ ISR(TCC1_OVF_vect)
 	{
 		TCC0_CTRLA = TC_CLKSEL_DIV64_gc;
 		TCC1_PER = 600;
-	}	
+	}
+}
+
+ISR(USARTE0_TXC_vect)
+{
+	Tx_Handler(&serialStruct);
+}
+
+ISR(USARTE0_RXC_vect)
+{
+	Rx_Handler(&serialStruct);
+}
+
+ISR(PORTF_INT0_vect)
+{
+	/* for when we use serial
+	USART_send(&serialStruct, "A falling edge was detected.");
+	*/
+	
+	PORTH_OUT = ++temp;
 }
 
 
 void main(void)
 {
 	unsigned long sClk, pClk;
+	char recieveString[100];
+	is1 = 0; //false
+	temp = 0;
 	
 	cli(); //
 	
@@ -45,7 +77,7 @@ void main(void)
 	* The current values of PER and CCA for this timer will result in the 38Khz oscillating signal needed
 	* in order to generate 1's and 0's that the reciever recognizes. Do not change these values.
 	*/
-	TCC0_CTRLA = TC_CLKSEL_DIV64_gc; //set prescaler to clk/64
+	TCC0_CTRLA = TC_CLKSEL_OFF_gc; //set timer to be off intially until serial input happens.
 	TCC0_CTRLB = 0x10 | TC_WGMODE_SS_gc; //turn on capture(CCAEN) and set waveform generation mode to PWM
 	TCC0_CTRLC = 0x00; //turn off compares
 	TCC0_CTRLD = 0x00; //turn off events
@@ -60,26 +92,71 @@ void main(void)
 	* .6ms of no signal. A 0 is represented with .6ms of 38Khz oscillating signal followed by .6ms of
 	* no signal.
 	*/
-	TCC1_CTRLA = TC_CLKSEL_DIV64_gc; //set prescaler to clk/64
+	TCC1_CTRLA = TC_CLKSEL_OFF_gc; //set timer to be off intially until serial input happens.
 	TCC1_CTRLB = TC_WGMODE_NORMAL_gc; //set waveform generation mode to normal
 	TCC1_CTRLC = 0x00; //turn off compares
 	TCC1_CTRLD = 0x00; //turn off events
 	TCC1_CTRLE = 0x00; //turn off byte mode
-	TCC1_PER = 600; //set the top of the period to overflow at 1.2ms
+	TCC1_PER = 600; //set the top of the period to overflow at 1.2ms. NOTE: to get .6ms set this to 300.
 	TCC1_INTCTRLA = 0x01; //set timer c1 overflow interrupts to low level
 	
 	/*
 	* Port C configuration
+	* Set direction to output
 	*/
 	PORTC_DIR = 0xFF;
-	PORTH_DIR = 0xFF;
+	
+	/*
+	* Port H configuration
+	* Just turn off LED's
+	*/
+	PORTH_DIR = 0xFF; //output dir for all pins
 	PORTH_OUT = 0x00;
+	
+	/*
+	* Serial set up
+	*/
+	//initialize the usart e0 for 57600 baud with 8 data bits, no parity, and 1 stop bit, interrpts on low (porth set to this for debugging purposes)
+	PORTH_OUT = USART_init(&serialStruct, 0xE0, pClk, (_USART_RXCIL_MED | _USART_TXCIL_MED), 576, -4, _USART_CHSZ_8BIT, _USART_PM_DISABLED, _USART_SM_1BIT);
+	USART_buffer_init(&serialStruct, 100, 100); //initialize the circular buffers
+	USART_enable(&serialStruct, USART_TXEN_bm | USART_RXEN_bm); //enable the USART
+	serialStruct.fOutMode = _OUTPUT_CRLF; //append a carriage return and a line feed to every output.
+	serialStruct.fInMode = _INPUT_CR | _INPUT_TTY; //echo input back to the terminal and set up for keyboard input.
+	
+	/*
+	* Port F configuration
+	*/
+	PORTF_DIR = 0x00; //input dir for all pins
+	PORTF_INTCTRL = 0x01; //turn on interrupt 0 with a low priority
+	PORTF_INT0MASK = 0x01; //mask so that only pin 0 can fire an interrupt;
+	PORTF_PIN0CTRL = 0x02; //set pin 0 to detect a falling edge as the receiver is active low.
 	
 	sei();
 		
     while(1)
     {
-        ;
+        if(serialStruct.serStatus & _USART_RX_DONE)
+		{
+			USART_read(&serialStruct, recieveString);
+			
+			switch(recieveString[0])
+			{
+				case '0':
+				TCC0_CTRLA = TC_CLKSEL_DIV64_gc;
+				TCC1_PER = 300;
+				TCC1_CTRLA = TC_CLKSEL_DIV64_gc;
+				USART_send(&serialStruct, "Sending a 0");
+				is1 = 0; //false
+				break;
+				case '1':
+				TCC0_CTRLA = TC_CLKSEL_DIV64_gc;
+				TCC1_PER = 600;
+				TCC1_CTRLA = TC_CLKSEL_DIV64_gc;
+				USART_send(&serialStruct, "Sending a 1");
+				is1 = 1; //true
+				break;
+			}
+		}		
     }
 
 }
