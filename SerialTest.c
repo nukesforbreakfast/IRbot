ebe8ea439d56e3e77f3ee8890161d3dd663227c2
Ryan Causey <rccausey@gmail.com>
2012-11-25 13:32:25 -0800

    The serial on the green board does not seem to work through USB. I cannot see an error in my code so I am committing this now and going to continue work on another branch.

--- SerialTest.c
+++ SerialTest.c
@@ -37,13 +37,15 @@ void main(void)
 	*/
 	PMIC_CTRL = PMIC_HILVLEN_bm | PMIC_MEDLVLEN_bm | PMIC_LOLVLEN_bm; //enable all levels of interrupts
 	
+	PORTH_DIR = 0xFF;
+	
 	/*
 	* Serial set up
 	*/
 	//initialize the usart d0 for 57600 baud with 8 data bits, no parity, and 1 stop bit, interrupts on low (porth set to this for debugging purposes)
 	USART_init(&serialStruct, 0xD0, pClk, (_USART_RXCIL_LO | _USART_TXCIL_LO), 576, -4, _USART_CHSZ_8BIT, _USART_PM_DISABLED, _USART_SM_1BIT);
 	USART_buffer_init(&serialStruct, 100, 100); //initialize the circular buffers
-	USART_enable(&serialStruct, USART_TXEN_bm | USART_RXEN_bm); //enable the USART
+	PORTH_OUT = USART_enable(&serialStruct, USART_TXEN_bm | USART_RXEN_bm); //enable the USART
 	serialStruct.fOutMode = _OUTPUT_CRLF; //append a carriage return and a line feed to every output.
 	serialStruct.fInMode = _INPUT_CR | _INPUT_TTY | _INPUT_ECHO; //echo input back to the terminal and set up for keyboard input.
 	
@@ -51,6 +53,6 @@ void main(void)
 	
 	while(1)
 	{
-		;
+		USART_send(&serialStruct, "Hey, am I working?");
 	}
 }
